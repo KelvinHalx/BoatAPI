4248ce2f5532167a620ce3fcad0242533211d446
KelvinHalx <kelvinwainaina03@gmail.com>
2021-10-25 15:24:54 +0300

    Update models.py

--- App/models.py
+++ App/models.py
@@ -4,16 +4,16 @@ from App import db, app
 class Boat(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String,nullable=False,unique=False)
-    boat_type = db.Column(db.String(40),nullable=False)
+    boattype = db.Column(db.String(40),nullable=False)
     length = db.Column(db.Integer,nullable=False)
 
     def __repr__(self):
-        return f"Boats(name = {name}, boat_type = {boat_type}, length = {lenght})"
+        return f"Boats(name = {name}, boattype = {boattype}, length = {lenght})"
 
 # Boat Schema
 class BoatSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'name', 'boat_type', 'length')
+    fields = ('id', 'name', 'boattype', 'length')
 
 #Init Schema
 boat_schema = BoatSchema()
