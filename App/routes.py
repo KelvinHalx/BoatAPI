475173037f92f250f61f5db92ec4c6f249379267
KelvinHalx <kelvinwainaina03@gmail.com>
2021-10-25 14:08:05 +0300

    Create routes.py

---
+++ App/routes.py
@@ -0,0 +1,54 @@
+import os
+from flask import render_template, url_for, flash, redirect, request, abort
+from App import app, db
+
+# Create a Boat
+@app.route("/boat",methods=['POST'])
+def add_boat():
+    name = request.json['name']
+    boat_type = request.json['boat_type']
+    length = request.json['length']
+
+    new_boat = Boat(name,boat_type,length)
+
+    db.session.add(boat)
+    db.session.commit()
+
+    return boat_schema.jsonify(new_boat)
+
+# Get All Boats
+@app.route('/boat', methods=['GET'])
+def get_boats():
+  all_boats = Boat.query.all()
+  result = boats_schema.dump(all_boats)
+  return jsonify(result.data)
+
+# Get Single Boat
+@app.route('/boat/<id>', methods=['GET'])
+def get_boatt(id):
+  boat = Boat.query.get(id)
+  return boat_schema.jsonify(product)
+
+# Update a Boat
+@app.route("/boat/<id>",methods=['PUT'])
+def update_boat():
+    boat = Boat.query.get(id)
+
+    name = request.json['name']
+    boat_type = request.json['boat_type']
+    length = request.json['length']
+
+    new_boat = Boat(name,boat_type,length)
+
+    db.session.commit()
+
+    return boat_schema.jsonify(new_boat)
+
+# Delete a Boat
+@app.route('/boat/<id>', methods=['DELETE'])
+def delete_boat(id):
+  boat = Boat.query.get(id)
+  db.session.delete(product)
+  db.session.commit()
+
+  return product_schema.jsonify(product)
